536b2422f0006aedf262439fd52bdbd3d9946d3f
Nauman Ya <nauman1225@gmail.com>
2026-05-15 05:10:12 +0100

    nvim-tree keymap change

--- nvim-scratch/lua/plugins/nvim-tree.lua
+++ nvim-scratch/lua/plugins/nvim-tree.lua
@@ -22,6 +22,27 @@ require("nvim-tree").setup({
   filters = {
     git_ignored = false,
   },
+  actions = {
+    change_dir = {
+      restrict_above_cwd = true,
+    },
+  },
+  on_attach = function(bufnr)
+    local api = require("nvim-tree.api")
+    local function opts(desc)
+      return {
+        desc = "nvim-tree: " .. desc,
+        buffer = bufnr,
+        noremap = true,
+        silent = true,
+        nowait = true,
+      }
+    end
+    api.config.mappings.default_on_attach(bufnr)
+    vim.keymap.set("n", "<CR>", api.tree.change_root_to_node, opts("CD"))
+  end,
 })
+
+-- BEGIN_ON_ATTACH_DEFAULT
 
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
