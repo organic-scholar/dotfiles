23ecfa26315228ae83d1546814be950e875c68b2
Nauman Ya <nyaseen@cisco.com>
2026-06-30 09:20:34 +0100

    helm_ls config

--- nvim/.config/nvim/lua/plugins/helm.lua
+++ nvim/.config/nvim/lua/plugins/helm.lua
@@ -2,6 +2,17 @@ return {
   {
     "qvalentin/helm-ls.nvim",
     branch = "main",
-    config = function() require("helm-ls").setup({}) end,
+    lazy = false,
+    opts = {
+      conceal_templates = {
+        -- enable the replacement of templates with virtual text of their current values
+        enabled = false, -- tree-sitter must be setup for this feature
+      },
+      indent_hints = {
+        -- enable hints for indent and nindent functions
+        enabled = false, -- tree-sitter must be setup for this feature
+      },
+    },
+    -- config = function() require("helm-ls").setup({}) end,
   },
 }
